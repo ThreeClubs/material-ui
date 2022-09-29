794e1641e4041a6ff4292aae3f96d1c75b1cab6f
Andrew Grenon <andrew.grenon@gmail.com>
2022-09-30 12:46:42 +1300

    Init Commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from "@mui/material/Button";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="button-container">
+        <Button variant="contained">
+          Contained
+        </Button>
+        <Button variant="outlined">
+          Outlined
+        </Button>
+        <Button variant="text">
+          Hover OVer Me
+        </Button>
+      </div>
+      <div className="button-container">
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+        <Button variant="outlined" color="error">
+          Error
+        </Button>
+        <Button variant="text" color="secondary">
+          secondary
+        </Button>
+      </div>
+    </>
   );
 }
 
